1cc39bde042a5b6e4292949227c3fbcf961fc306
OliverWich <kontakt@oliver-wich.de>
2024-10-09 11:15:38 +0200

    fix: include shipping in build site

--- build-all.sh
+++ build-all.sh
@@ -5,6 +5,7 @@ asyncapi bundle common/schemas.yaml offering/asyncapi.yaml > offering/final.yaml
 asyncapi bundle common/schemas.yaml shoppingBasket/asyncapi.yaml > shoppingBasket/final.yaml
 asyncapi bundle common/schemas.yaml payment/asyncapi.yaml > payment/final.yaml
 asyncapi bundle common/schemas.yaml product/asyncapi.yaml > product/final.yaml
+asyncapi bundle common/schemas.yaml shipping/asyncapi.yaml > shipping/final.yaml
 
 asyncapi generate fromTemplate customer/final.yaml @asyncapi/html-template -o ./customer/
 asyncapi generate fromTemplate order/final.yaml @asyncapi/html-template -o ./order/
@@ -12,4 +13,5 @@ asyncapi generate fromTemplate offering/final.yaml @asyncapi/html-template -o ./
 asyncapi generate fromTemplate shoppingBasket/final.yaml @asyncapi/html-template -o ./shoppingBasket/
 asyncapi generate fromTemplate payment/final.yaml @asyncapi/html-template -o ./payment/
 asyncapi generate fromTemplate product/final.yaml @asyncapi/html-template -o ./product/
+asyncapi generate fromTemplate shipping/final.yaml @asyncapi/html-template -o ./shipping/
 
